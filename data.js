b2d52491951f3b7473e2266597074783f1520f22
Roberto Ramirez <robertrafyt2007@gmail.com>
2020-03-23 10:37:12 -0600

    new design

---
+++ data.js
@@ -0,0 +1,48 @@
+const main = {
+  name: 'Roberto Ramírez',
+  mail: 'robertrafyt2007@gmail.com',
+  img: './assets/img/user.jpg',
+  role: ['Junior Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: 'https://robertraf.github.io/'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/robertraf'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/roberto-ram%C3%ADrez-b753b9189/'
+    },
+    /*{
+      name: 'StackOverflow',
+      iconName: 'logo-stackoverflow',
+      link: 'https://stackoverflow.com/users/6940525/sivanesh-s'
+    },*/
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/robert_raf'
+    },
+    /*{
+      name: 'npm',
+      iconName: 'logo-npm',
+      link: 'https://www.npmjs.com/~sivanesh_s'
+    },*/
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'robertrafyt2007@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blogs', link: '#blogs' }
+  ]
+};
